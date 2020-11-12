c435b398afe0e809e0855a032b127ae72d4df0a0
Tim Thabt <haithamthabt@gmail.com>
2020-11-11 22:49:55 -0500

    edited setup.py and readme

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
  
  
 setup(
   name='numetrics',
-  version='0.0.1',
+  version='0.0.3',
   description='Package For Numerai users to check their models performance',
-  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  long_description=long_description + '\n\n' + open('CHANGELOG.txt').read(),
+  long_description_content_type="text/markdown",
   url='',  
   author='Tim Thabt',
   author_email='haithamthabt@hotmail.com',
